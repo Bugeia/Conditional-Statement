5390023215f736b86e19c24122f9754a630bb43f
bugei <yanjunyang@mail.usf.edu>
2019-01-20 10:32:41 -0500

    Version 2

--- Conditional Statement/Program.cs
+++ Conditional Statement/Program.cs
@@ -6,7 +6,30 @@ namespace Conditional_Statement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How old are you?");
+            try
+            {
+                string input = Console.ReadLine();
+                int year = int.Parse(input);
+
+                if (year < 18)
+                {
+                    Console.WriteLine("Minor.");
+                }
+                else if ((year > 17) && (year < 65))
+                {
+                    Console.WriteLine("Adult.");
+                }
+                else
+                {
+                    Console.WriteLine("Senior.");
+                }
+            }// end of try
+            catch
+            {
+                Console.WriteLine("Please enter an integer.");
+                Console.ReadKey(true);
+            }// end of catch
         }
     }
 }
